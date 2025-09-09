6e79fcfc6dd438e6e033aeab5f0ac391da195f98
botflowlab <botflowlab@gmail.com>
2025-09-09 16:21:02 -0600

    Updated HomeHero.tsx

--- src/components/sections/home/HomeHero.tsx
+++ src/components/sections/home/HomeHero.tsx
@@ -15,7 +15,7 @@ export default function HomeHero() {
       color: '#FBF2E9',
       textAlign: 'center',
       padding: '2rem',
-      opacity: '80'
+      opacity: '30'
     }}>
       <h1>Find Your Dream Home</h1>
       <p>Discover exceptional properties with Henkel-Williams Real Estate. We help you buy, sell, and invest in the perfect property.</p>
