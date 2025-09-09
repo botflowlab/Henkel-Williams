3da8f34a75da0c657da118ced5026119620adf69
botflowlab <botflowlab@gmail.com>
2025-09-09 16:21:21 -0600

    Updated HomeHero.tsx

--- src/components/sections/home/HomeHero.tsx
+++ src/components/sections/home/HomeHero.tsx
@@ -12,7 +12,6 @@ export default function HomeHero() {
       flexDirection: 'column',
       justifyContent: 'center',
       alignItems: 'center',
-      color: '#FBF2E9',
       textAlign: 'center',
       padding: '2rem',
       opacity: '70%'
