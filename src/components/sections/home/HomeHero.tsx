3638cf8ecaebd2f40eaeee3823c57e76a1ce9259
botflowlab <botflowlab@gmail.com>
2025-09-09 16:14:51 -0600

    Add background image to HomeHero

--- src/components/sections/home/HomeHero.tsx
+++ src/components/sections/home/HomeHero.tsx
@@ -2,7 +2,20 @@ import Button from '../../ui/Button';
 
 export default function HomeHero() {
   return (
-    <section>
+    <section style={{
+      backgroundImage: 'url(/src/assets/images/template website.jpg)',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      minHeight: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      color: '#FBF2E9',
+      textAlign: 'center',
+      padding: '2rem'
+    }}>
       <h1>Find Your Dream Home</h1>
       <p>Discover exceptional properties with Henkel-Williams Real Estate. We help you buy, sell, and invest in the perfect property.</p>
       <button>Get Started</button>
