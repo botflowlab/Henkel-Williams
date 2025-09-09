7ab6f405da264f8c3e988fa29fd6d3fd1075e2a1
botflowlab <botflowlab@gmail.com>
2025-09-09 16:30:22 -0600

    Updated Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -38,6 +38,7 @@ export default function Header() {
       position: 'fixed',
       left: '0',
       right: '0',
+      top: '0',
       zIndex: '1000',
       flexWrap: 'wrap',
       minHeight: '70px',
