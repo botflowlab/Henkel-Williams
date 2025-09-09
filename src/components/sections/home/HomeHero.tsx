c554d7391fc55fab2ad0dde77abd332b01bd9ac9
botflowlab <botflowlab@gmail.com>
2025-09-09 16:27:04 -0600

    Remove white margins from HomeHero background

--- src/components/sections/home/HomeHero.tsx
+++ src/components/sections/home/HomeHero.tsx
@@ -3,6 +3,14 @@ import Button from '../../ui/Button';
 export default function HomeHero() {
   return (
     <section style={{
+      margin: '0',
+      padding: '0',
+      width: '100vw',
+      position: 'relative',
+      left: '50%',
+      right: '50%',
+      marginLeft: '-50vw',
+      marginRight: '-50vw',
       backgroundImage: 'url(/src/assets/images/templatewebsite.jpg)',
       backgroundSize: 'cover',
       backgroundPosition: 'center',
@@ -14,7 +22,7 @@ export default function HomeHero() {
       alignItems: 'center',
       textAlign: 'center',
       color: '#FBF2E9'
-    }}>
+      top: '0'
       <h1>Find Your Dream Home</h1>
       <p>Discover exceptional properties with Henkel-Williams Real Estate. We help you buy, sell, and invest in the perfect property.</p>
       <button>Get Started</button>
